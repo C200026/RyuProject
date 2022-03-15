d978f29e0014a9233e65776ef899b1ceead97b6b
mizuhara <c200026@ccg.ac.jp>
2022-03-15 21:41:07 +0900

    responsive design

--- template.js
+++ template.js
@@ -1,5 +1,5 @@
 function header(){
-  var header = '<div id = "header-title">Gallery R</div>';
+  var header = ""
   document.write(header);
 }
 
@@ -11,26 +11,29 @@ function footer(){
 
 function navbar(){
     var navbar = "";
-    navbar = '<nav class="navbar navbar-expand navbar-light">\
+    navbar = '<nav class="navbar navbar-expand-lg navbar-light d-flex">\
     <div class="container-fluid">\
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">\
+      <a class="navbar-brand flex-fill">\
+        <img src="img/logo02.png" alt="Gallery R" height="70">\
+      </a>\
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="true" aria-label="Toggle navigation">\
         <span class="navbar-toggler-icon"></span>\
       </button>\
-      <div class="collapse navbar-collapse" id="navbarNav">\
-        <ul class="navbar-nav">\
-          <li class="nav-item">\
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo02">\
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">\
+          <li class="nav-item p-2 flex-fill">\
             <a class="nav-link" href="index.html">Home</a>\
           </li>\
-          <li class="nav-item">\
+          <li class="nav-item p-2 flex-fill">\
             <a class="nav-link" href="profile.html">Profile</a>\
           </li>\
-          <li class="nav-item">\
+          <li class="nav-item p-2 flex-fill">\
             <a class="nav-link" href="picture.html">Picture</a>\
           </li>\
-          <li class="nav-item">\
+          <li class="nav-item p-2 flex-fill">\
             <a class="nav-link" href="illustration.html">Illustration</a>\
           </li>\
-          <li class="nav-item">\
+          <li class="nav-item p-2 flex-fill">\
             <a class="nav-link" href="award.html">Award-winning Work</a>\
           </li>\
         </ul>\
